dda3602187719f399e1624087528d8e4545f576b
matthew theseira <matthew.theseira@gmail.com>
2025-06-11 12:45:15 +1000

    added a sidebar

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,19 @@
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-const map = L.map("map").setView([0, 0], 2);
+const map = L.map("map").setView([-33.805, 511.09], 13);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution: "&copy; OpenStreetMap contributors",
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+//Add zoom control
+L.control
+  .zoom({
+    position: "bottomright",
+  })
+  .addTo(map);
 
 // Example click event to add marker
 map.on("click", function (e) {
@@ -14,4 +22,12 @@ map.on("click", function (e) {
     .addTo(map)
     .bindPopup(`Lat: ${lat}, Lng: ${lng}`)
     .openPopup();
+});
+
+const toggleButton = document.querySelector(".sidebar .toggle");
+const sidebar = document.querySelector(".sidebar");
+
+toggleButton.addEventListener("click", () => {
+  sidebar.classList.toggle("collapsed");
+  toggleButton.classList.toggle("rotated");
 });
